a5cdfebd3f15d90760ddd1bea96e5318b6027c1b
Hao Wei Lim <hwl11212003@gmail.com>
2025-12-27 12:11:29 +0000

    created state for random dice values

--- App.jsx
+++ App.jsx
@@ -1,8 +1,25 @@
+import Dice from "./Dice"
+import { useState } from "react"
 
 export default function App(){
+
+    const [ diceValues, setDiceValues ] = useState(generateAllNewDice())
+
+    function generateAllNewDice(){
+        const arr = []
+        for(let i = 0; i < 10; i ++){
+            arr.push(Math.floor(Math.random() * 6) + 1);
+        }
+        return arr
+    }
+
     return(
         <main>
-            test
+            <div className="dice-container">
+                {diceValues.map((value, index) => (
+                    <Dice key={index} value={value} />
+                ))}
+            </div>
         </main>
     )
 }
---
+++ Dice.jsx
@@ -0,0 +1,6 @@
+
+export default function Dice(props){
+    return(
+        <button>{props.value}</button>
+    )
+}
