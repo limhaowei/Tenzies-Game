a320762f4b5b2b140d661cfde8a7a744320de976
Hao Wei Lim <hwl11212003@gmail.com>
2025-12-30 19:55:52 +0000

    more accessibility tech

--- Dice.jsx
+++ Dice.jsx
@@ -8,6 +8,9 @@ export default function Dice(props){
             onClick={() => props.hold(props.id)} 
             //onClick={props.hold}
             style={styles}
+            aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+            aria-pressed={props.held}
         >{props.value}</button>
     )
 }
